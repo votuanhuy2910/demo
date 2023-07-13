23880e0d3420a4b344082f66f51d824dab0d2a07
votuanhuy2910 <votuanhuy2910@gmail.com>
2023-07-13 21:57:13 +0700

    update

---
+++ errors/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <!-- <div class="container-header">
+            <div class="logo">
+                <img src="https://ueh.edu.vn/images/logo-header.png" alt="">
+            </div>
+
+            <div class="title">
+                <h2>TUYEN SINH 2023</h2>
+                <h2>DAI HOC KINH TE THANH PHO HO CHI MINH</h2>
+            </div>
+
+            <div class="home">
+                <a href=""><i class="bi bi-house-door"></i>Trang chu</a>
+            </div>
+        </div> -->
+        <nav class="navbar">
+            <div class="container">
+                <a class="navbar py-0" href="#">
+                    <img src="https://ueh.edu.vn/images/logo-header.png" alt="" width="63" height="36">
+                </a>
+
+                <h2 class="navbar py-0 text-white text-center">
+                    tuyen sinh 2023
+                    <br>
+                    dai hoc kinh te thanh pho ho chi minh
+                </h2>
+
+                <a class="navbar text-white fs-3 py-0" href="#">
+                    <i class="bi bi-house-door me-2"></i>
+                    trang chu
+                </a>
+            </div>
+        </nav>
+    </header>
---
+++ errors/index.php
@@ -0,0 +1,15 @@
+<?php require "header.php"; ?>
+
+<main>
+    <div class="container d-flex flex-column">
+        <div class="infor-person border-bottom border-2 mb-3">
+            <h1>THONG TIN CA NHAN</h1>
+        </div>
+        <div class="infor">
+            <?php require "infor.php" ?>
+        </div>
+    </div>
+</main>
+
+
+<?php require "footer.php" ?>
